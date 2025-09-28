81cc08d9a5210a7fa561f4185735c469fa15c66e
andrewplot <andrewplotplot@gmail.com>
2025-09-28 15:53:00 -0400

    commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import subprocess
 import PyPDF2
 import re
 
+#Text Extraction and Processing
 def extract_pdf_text(doc):
     reader = PyPDF2.PdfFileReader(doc)
     text = ""
@@ -15,3 +17,14 @@ def preprocess_text(text):
     text = re.sub(r'[^A-Za-z0-9\+\#\.\- ]+', ' ', text)  # replace all characters outside of set with spaces
     text = re.sub(r'\s+', ' ', text).strip() #remove all extra whitespaces
     return text
+
+#Keyword Extraction
+
+
+
+
+
+def main():
+    resume_file_path = "/Users/aplotnik/Downloads/Resume-5.pdf"
+    subprocess.run(["open", resume_file_path])
+main() 
